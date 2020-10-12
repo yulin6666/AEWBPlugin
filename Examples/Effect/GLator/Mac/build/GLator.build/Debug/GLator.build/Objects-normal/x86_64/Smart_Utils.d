d3a3ae447f233bb67a688f654b4e7bf489ee0ddb
王璟鑫 <wangjingxin@wangjingxindeMacBook-Pro.local>
2020-10-12 14:48:43 +0800

    添加示例多个target

---
+++ Examples/Effect/GLator/Mac/build/GLator.build/Debug/GLator.build/Objects-normal/x86_64/Smart_Utils.d
@@ -0,0 +1,9 @@
+dependencies: \
+  /Users/wangjingxin/Desktop/AfterEffectsSDK/Examples/Util/Smart_Utils.cpp \
+  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.15.sdk/System/Library/Frameworks/Cocoa.framework/Headers/Cocoa.h \
+  ../../../Util/Smart_Utils.h \
+  /Users/wangjingxin/Desktop/AfterEffectsSDK/Examples/Headers/AE_Effect.h \
+  ../../../Headers/A.h ../../../Headers/adobesdk/config/PreConfig.h \
+  ../../../Headers/adobesdk/config/PostConfig.h \
+  ../../../Headers/SP/SPTypes.h ../../../Headers/SP/SPConfig.h \
+  ../../../Headers/SP/SPErrorCodes.h
